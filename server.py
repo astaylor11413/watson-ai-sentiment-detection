f40851d47a5cf3660d0e640fdf644c711ccdd485
astaylor11413 <astaylor140@gmail.com>
2025-08-11 22:41:40 -0400

    Static code analysis complete with PyLint

--- server.py
+++ server.py
@@ -1,3 +1,11 @@
+"""
+This module provides route handling for all routes accessible within the app
+
+It includes functions for 
+- extracting emotion data from responses
+- rendering the front-end side of the application
+- executing a connection to the localhost:5000 server
+"""
 from flask import Flask,request,render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,21 +13,28 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detector():
+    """
+    This function extracts scores of emotion context data when a submission is executed
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     dominant_emotion = response['dominant_emotion']
-    if(dominant_emotion is None):
+    if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        anger = response['anger']
-        disgust = response['disgust']
-        fear = response['fear']
-        joy = response['joy']
-        sadness = response['sadness']
-        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    message_1 = f"{anger}, 'disgust': {disgust},'fear': {fear}, 'joy': {joy},"
+    message_2 = f" and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return "For the given statement, the system response is 'anger': " + message_1 + message_2
 
 @app.route("/")
 def render_index_page():
+    """
+    This function allows users to interact with the app when using the main home directory
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
