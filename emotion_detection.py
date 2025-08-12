1cee5d371b6a8554658d9f4a18732ab1654909d2
astaylor11413 <astaylor140@gmail.com>
2025-08-11 20:46:20 -0400

    completed output formatting for json objects

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,23 +1,25 @@
-import json
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json = myobj, headers=header) 
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers= header)
     formatted_response = json.loads(str(response.text))
-    anger = formatted_response['emotionPredections']['emotion']['anger']
-    disgust = formatted_response['emotionPredections']['emotion']['disgust']
-    fear = formatted_response['emotionPredections']['emotion']['fear']
-    joy = formatted_response['emotionPredections']['emotion']['joy']
-    sadness = formatted_response['emotionPredections']['emotion']['sadness']
-    dominant_emotion = max(anger,disgust,fear,joy,sadness)
-    return {'anger':anger,
-            'disgust':disgust,
-           'fear':fear,
-            'joy':joy,
-            'sadness':sadness, 
-            'dominant_emotion':dominant_emotion} 
-    
-    
+    emotions_array = formatted_response["emotionPredictions"]
+    emotions_dict = emotions_array[0]
+    anger = emotions_dict["emotion"]["anger"]
+    disgust = emotions_dict["emotion"]["disgust"]
+    fear = emotions_dict["emotion"]["fear"]
+    joy = emotions_dict["emotion"]["joy"]
+    sadness = emotions_dict["emotion"]["sadness"]
+    max_score = max(anger, disgust, fear, joy, sadness)
+    # To find the first key with the target value
+    dominant_emotion = None
+    for key, value in emotions_dict["emotion"].items():
+        if value == max_score:
+            dominant_emotion = key
+            break  # Stop after finding the first match
+    return {"anger" : anger, "disgust" : disgust,"fear" : fear,
+            "joy" : joy,"sadness" : sadness,"dominant_emotion" : dominant_emotion}
